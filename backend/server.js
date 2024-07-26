720c3ac2aecf3caed86817a279dd5477b674ed92
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-07-26 20:04:49 +0300

    Add Authentication Route Handler To The Server

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,20 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+app.post("/authenticate", async (req, res) => {
+  const username = req.body.username;
+
+  try {
+    let chatEnginRes = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "60503f04-22c9-48d1-9ac9-2cbe8e777698" } }
+    );
+    return res.status(chatEnginRes.status).json(chatEnginRes.data);
+  } catch (err) {
+    return res.status(err.response.status).json(err.response.data);
+  }
+});
 app.listen(PORT, (req, res) => {
   console.log(`Server listen to port ${PORT}`);
 });
